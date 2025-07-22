7a06c5f388539114508529cf6abeb104799eb610
Michal Lulco <lulco@efabrica.sk>
2025-07-22 10:20:38 +0200

    Fixed create for version

--- src/ExtensionsFinder.php
+++ src/ExtensionsFinder.php
@@ -5,13 +5,14 @@ namespace ExtensionsFinder;
 use PhpParser\NodeTraverser;
 use PhpParser\NodeVisitor\NameResolver;
 use PhpParser\ParserFactory;
+use PhpParser\PhpVersion;
 use Symfony\Component\Finder\Finder;
 
 class ExtensionsFinder
 {
     public function find(array $dirs): array
     {
-        $parser = (new ParserFactory())->createForVersion('7.4');
+        $parser = (new ParserFactory())->createForVersion(PhpVersion::fromString('7.4'));
         $traverser = new NodeTraverser();
         $traverser->addVisitor(new NameResolver());
         $requiredExtensions = [];
