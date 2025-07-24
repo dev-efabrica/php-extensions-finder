0b0ea24d0fd7f48a6ab58cb34bd753b06abde1da
Martin Mystik Jonáš <mpj.jonas@gmail.com>
2025-07-24 12:06:49 +0200

    Use host PHP version in parser

--- src/ExtensionsFinder.php
+++ src/ExtensionsFinder.php
@@ -5,14 +5,13 @@ namespace ExtensionsFinder;
 use PhpParser\NodeTraverser;
 use PhpParser\NodeVisitor\NameResolver;
 use PhpParser\ParserFactory;
-use PhpParser\PhpVersion;
 use Symfony\Component\Finder\Finder;
 
 class ExtensionsFinder
 {
     public function find(array $dirs): array
     {
-        $parser = (new ParserFactory())->createForVersion(PhpVersion::fromString('7.4'));
+        $parser = (new ParserFactory())->createForHostVersion();
         $traverser = new NodeTraverser();
         $traverser->addVisitor(new NameResolver());
         $requiredExtensions = [];
